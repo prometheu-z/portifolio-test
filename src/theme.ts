7f7888d47b46932b4b3bb93bf745b729c2828c3d
prometheu-z <gabrielsampaiodejesus01042007@gmail.com>
2025-07-26 17:37:08 -0300

    feat: configurando o thema do material ui e a base do css

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#526280',
+    },
+    secondary: {
+      main: '#5a3e8fff',
+    },
+  },
+  typography:{
+    fontFamily: "Nunito"
+  }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme
